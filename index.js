e7df60f59b6cef45ea3159c53011f48a6fe0b4a8
Labonya <1aklimakter@gmail.com>
2023-11-06 22:36:37 +0600

    I have added all food data in mongodb

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express")
 const cors = require("cors")
 const app = express()
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require("dotenv").config();
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middleware
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: ["http://localhost:5174"],
+  credentials: true
+}))
+app.use(cookieParser())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9q6ocyc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,26 +27,92 @@ const client = new MongoClient(uri, {
   }
 });
 
+//middleware 
+const verifyToken =(req, res,next)=>{
+  const token =req?.cookies?.token
+  console.log("token int the middleware", token);
+  //jodi token na pai
+  if(!token){
+    return res.status(401).send({message:"unAuthorized access"})
+  }
+  jwt.verify(token, process.env.SECRET_KEY,(error, decoded)=>{
+    if(error){
+      return res.status(401).send({message:"unAuthorized access"})
+    }
+    res.user = decoded
+    next()
+  })
+}
+
+
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
+    const foodCollection = client.db("shareFoodDB").collection("foodCollection")
+    
+    //auth related api
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      console.log("user for token", user);
+      const token = jwt.sign(user, process.env.SECRET_KEY, { expiresIn: "10h" })
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+      }).send({ success:true });
+    })
+
+  
+    //user logout ar jonno api create korbo 
+   app.post("/logout", async(req, res)=>{
+    const user = req.body;
+    res.clearCookie('token', {maxAge: 0}).send({success : true})
+   })
+
+    //all food api
+    app.post("/foodAdd", async (req, res) => {
+      const food = req.body;
+      const result = await foodCollection.insertOne(food)
+      res.send(result)
+    })
+
+    //all foods get api
+    app.get("/getFood", verifyToken, async (req, res) => {
+      console.log("cook cookies",req.cookies);
+      const cursor = foodCollection.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    //find single food 
+    app.get("/getFood/:id", verifyToken, async (req, res) => {
+      console.log(req.cookies);
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await foodCollection.findOne(query)
+      res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 
-app.get("/", (req,res)=>{
-    res.send("Assignment 11 all data is coming soon!")
+app.get("/", (req, res) => {
+  res.send("Assignment 11 all data is coming soon!")
 })
 
-app.listen(port, (req, res)=>{
-    console.log(`This is assignment 11 port ,${port}`);
+app.listen(port, (req, res) => {
+  console.log(`This is assignment 11 port ,${port}`);
 })
